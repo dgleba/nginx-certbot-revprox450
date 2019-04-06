cb1005cb628752722d911703715dd466bd5941f5
David Gleba <dgleba@gmail.com>
2019-04-06 16:39:10 +0000

    dg2

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-domains=(example.com www.example.com)
+domains=(dg76.ga www.dg76.ga)
+
 rsa_key_size=4096
 data_path="./data/certbot"
-email="" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+
+email="dgleba@gmail.com" # Adding a valid address is strongly recommended
+
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
